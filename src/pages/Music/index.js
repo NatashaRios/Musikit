cd20ae54f64c0194aab404289b017c1569183dea
Natasha Rios <natu.rios@hotmail.com>
2020-10-15 20:29:43 -0300

    apis

--- src/pages/Music/index.js
+++ src/pages/Music/index.js
@@ -1,4 +1,4 @@
-import React, {useEffect} from 'react';
+import React, {useEffect, useState} from 'react';
 import { useLocation, useParams } from 'react-router-dom'; 
 
 import './styles.scss';
@@ -8,41 +8,37 @@ function Music(){
   const location = useLocation();
   const [fullHash] = location.hash.split('&');
   const [, accessToken] = fullHash.split('=');
+
+  const [dataToken, setDataToken] = useState({});
+  const [dataAlbumsMe, setDataAlbumsMe] = useState({});
+  const [dataPlaylistsMe, setDataPlaylistsMe] = useState({});
+  const [dataAlbums, setDataAlbums] = useState({});
+  
   useEffect(() => {
-    
-    fetchData()
+    fetchData('https://api.spotify.com/v1/me', setDataToken);
+    fetchData('https://api.spotify.com/v1/me/albums', setDataAlbumsMe);
+    fetchData('https://api.spotify.com/v1/me/playlists', setDataPlaylistsMe);
+    fetchData('https://api.spotify.com/v1/albums/?ids=6ZG5lRT77aJ3btmArcykra,4PgleR09JVnm3zY1fW3XBA,47BiFcV59TQi2s9SkBo2pb,7L6gLnSJBTU0tOneX0Ol91,4m2880jivSbbyEGAKfITCa,1Rv9WRKyYhFaGbuYDaQunN,1VW1MFNstaJuygaoTPkdCk,6ylFfzx32ICw4L1A7YWNLN,355bjCHzRJztCzaG5Za4gq,3g09ln1DeyjtPhlITh8DzT,29m6DinzdaD0OPqWKGyMdz,1klALx0u4AavZNEvC4LrTL,791GBXI2YhfSdzn0ARQzlj', setDataAlbums)
   }, [])
 
-  async function fetchData(){
-    
-    const dataToken = await fetch('https://api.spotify.com/v1/me', {
+  async function fetchData(url, setter){
+    const getData = await fetch(url, {
       headers: {
         'Authorization': `Bearer ${accessToken}`
       }
     });
-    const dataTokenJson = await dataToken.json();
-    console.log(dataTokenJson);
-
-    const dataAlbums = await fetch('https://api.spotify.com/v1/me/albums', {
-      headers: {
-        'Authorization': `Bearer ${accessToken}`
-      }
-    });
-    const dataAlbumsJson = await dataAlbums.json();
-    console.log(dataAlbumsJson)
-
-    const dataPlaylists = await fetch('https://api.spotify.com/v1/me/playlists', {
-      headers: {
-        'Authorization': `Bearer ${accessToken}`
-      }
-    });
-    const dataPlaylistsJson = await dataPlaylists.json();
-    console.log(dataPlaylistsJson)
-
-    
+      const json = await getData.json();
+      setter(json)
   }
-  
 
+  const info = {
+    token: dataToken,
+    albumsMe: dataAlbumsMe,
+    playlistsMe: dataPlaylistsMe,
+    albums: dataAlbums
+  }
+  console.log(info.token)
+  console.log(info.albums)
   return(
     <>
     <p>Hola music</p>
