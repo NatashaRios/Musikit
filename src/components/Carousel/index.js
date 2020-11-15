926f8bab98c5981e0b231ae8842bcfb0a96e29ee
Natasha Rios <natu.rios@hotmail.com>
2020-11-14 21:52:57 -0300

    ruta dinamica de lo clickeado

--- src/components/Carousel/index.js
+++ src/components/Carousel/index.js
@@ -3,75 +3,85 @@ import { Link } from 'react-router-dom';
 import './styles.scss';
 
 function Carousel({ carousel, infoAlbums, infoRecently, infoPodcast, infoArtist, infoAlbumsMe, infoPlaylists }){
-  
+ //console.log(infoAlbumsMe)
   return(
     <>
       {carousel && (
         <>
           {infoAlbums && (
-            <Link to={`/playlist/${infoAlbums.artists[0].name}`}>
-            <div className='carousel-content'>
-              <img className='carousel-img-computer carousel-img' src={infoAlbums.images[0].url} alt={infoAlbums.artists[0].name}/>
-              <img className='carousel-img-ipad carousel-img' src={infoAlbums.images[1].url} alt={infoAlbums.artists[0].name}/>
-              <img className='carousel-img-mobile carousel-img' src={infoAlbums.images[2].url} alt={infoAlbums.artists[0].name}/>
-              <div className='carousel-text'>
-                <p className='carousel-artist'>{infoAlbums.artists[0].name}</p>
-                <p className='carousel-album'>{infoAlbums.name}</p>
+            <Link to={`/playlist/${infoAlbums.uri}`}>
+              <div className='carousel-content'>
+                <img className='carousel-img-computer carousel-img' src={infoAlbums.images[0].url} alt={infoAlbums.artists[0].name}/>
+                <img className='carousel-img-ipad carousel-img' src={infoAlbums.images[1].url} alt={infoAlbums.artists[0].name}/>
+                <img className='carousel-img-mobile carousel-img' src={infoAlbums.images[2].url} alt={infoAlbums.artists[0].name}/>
+                <div className='carousel-text'>
+                  <p className='carousel-artist'>{infoAlbums.artists[0].name}</p>
+                  <p className='carousel-album'>{infoAlbums.name}</p>
+                </div>
               </div>
-            </div>
             </Link>
           )}
           {infoRecently && (
-            <div className='carousel-content'>
-              <img className='carousel-img-computer carousel-img' src={infoRecently.track.album.images[0].url} alt={infoRecently.track.artists[0].name}/>
-              <img className='carousel-img-ipad carousel-img' src={infoRecently.track.album.images[1].url} alt={infoRecently.track.artists[0].name}/>
-              <img className='carousel-img-mobile carousel-img' src={infoRecently.track.album.images[2].url} alt={infoRecently.track.artists[0].name}/>
-              <div className='carousel-text'>
-                <p className='carousel-artist'>{infoRecently.track.artists[0].name}</p>
-                <p className='carousel-album'>{infoRecently.track.album.name}</p>
+            <Link to={`/playlist/${infoRecently.context.uri}`}>
+              <div className='carousel-content'>
+                <img className='carousel-img-computer carousel-img' src={infoRecently.track.album.images[0].url} alt={infoRecently.track.artists[0].name}/>
+                <img className='carousel-img-ipad carousel-img' src={infoRecently.track.album.images[1].url} alt={infoRecently.track.artists[0].name}/>
+                <img className='carousel-img-mobile carousel-img' src={infoRecently.track.album.images[2].url} alt={infoRecently.track.artists[0].name}/>
+                <div className='carousel-text'>
+                  <p className='carousel-artist'>{infoRecently.track.artists[0].name}</p>
+                  <p className='carousel-album'>{infoRecently.track.album.name}</p>
+                </div>
               </div>
-          </div>
+            </Link>
           )}
           {infoPodcast && (
-            <div className='carousel-content'>
-              <img className='carousel-img-computer carousel-img' src={infoPodcast.images[0].url} alt={infoPodcast.name} />
-              <img className='carousel-img-ipad carousel-img' src={infoPodcast.images[1].url} alt={infoPodcast.name}/>
-              <img className='carousel-img-mobile carousel-img' src={infoPodcast.images[2].url} alt={infoPodcast.name} />
-              <div className='carousel-text'>
-                <p className='carousel-artist'>{infoPodcast.name}</p>
+            <Link to={`/playlist/${infoPodcast.uri}`}>
+              <div className='carousel-content'>
+                <img className='carousel-img-computer carousel-img' src={infoPodcast.images[0].url} alt={infoPodcast.name} />
+                <img className='carousel-img-ipad carousel-img' src={infoPodcast.images[1].url} alt={infoPodcast.name}/>
+                <img className='carousel-img-mobile carousel-img' src={infoPodcast.images[2].url} alt={infoPodcast.name} />
+                <div className='carousel-text'>
+                  <p className='carousel-artist'>{infoPodcast.name}</p>
+                </div>
               </div>
-            </div>
+            </Link>
           )}
           {infoArtist && (
-            <div className='carousel-content'>
-              <img className='carousel-img-computer carousel-img' src={infoArtist.images[0].url} alt={infoArtist.name}/>
-              <img className='carousel-img-ipad carousel-img' src={infoArtist.images[1].url} alt={infoArtist.name}/>
-              <img className='carousel-img-mobile carousel-img' src={infoArtist.images[2].url} alt={infoArtist.name}/>
-              <div className='carousel-text'>
-                <p className='carousel-artist'>{infoArtist.name}</p>
+            <Link to={`/playlist/${infoArtist.uri}`}>
+              <div className='carousel-content'>
+                <img className='carousel-img-computer carousel-img' src={infoArtist.images[0].url} alt={infoArtist.name}/>
+                <img className='carousel-img-ipad carousel-img' src={infoArtist.images[1].url} alt={infoArtist.name}/>
+                <img className='carousel-img-mobile carousel-img' src={infoArtist.images[2].url} alt={infoArtist.name}/>
+                <div className='carousel-text'>
+                  <p className='carousel-artist'>{infoArtist.name}</p>
+                </div>
               </div>
-          </div>
+          </Link>
           )}
           {infoAlbumsMe && (
-            <div className='carousel-content'>
-              <img className='carousel-img-computer carousel-img' src={infoAlbumsMe.album.images[0].url} alt={infoAlbumsMe.album.artists[0].name}/>
-              <img className='carousel-img-ipad carousel-img' src={infoAlbumsMe.album.images[1].url} alt={infoAlbumsMe.album.artists[0].name}/>
-              <img className='carousel-img-mobile carousel-img' src={infoAlbumsMe.album.images[2].url} alt={infoAlbumsMe.album.artists[0].name}/>
-              <div className='carousel-text'>
-                <p className='carousel-artist'>{infoAlbumsMe.album.artists[0].name}</p>
-                <p className='carousel-album'>{infoAlbumsMe.album.name}</p>
+            <Link to={`/playlist/${infoAlbumsMe.album.uri}`}>
+              <div className='carousel-content'>
+                <img className='carousel-img-computer carousel-img' src={infoAlbumsMe.album.images[0].url} alt={infoAlbumsMe.album.artists[0].name}/>
+                <img className='carousel-img-ipad carousel-img' src={infoAlbumsMe.album.images[1].url} alt={infoAlbumsMe.album.artists[0].name}/>
+                <img className='carousel-img-mobile carousel-img' src={infoAlbumsMe.album.images[2].url} alt={infoAlbumsMe.album.artists[0].name}/>
+                <div className='carousel-text'>
+                  <p className='carousel-artist'>{infoAlbumsMe.album.artists[0].name}</p>
+                  <p className='carousel-album'>{infoAlbumsMe.album.name}</p>
+                </div>
               </div>
-            </div>
+            </Link>
           )}
           {infoPlaylists && (
-            <div className='carousel-content'>
-              <img className='carousel-img-computer carousel-img' src={infoPlaylists.images[0].url} alt={infoPlaylists.name}/>
-              <img className='carousel-img-ipad carousel-img' src={infoPlaylists.images[0].url} alt={infoPlaylists.name}/>
-              <img className='carousel-img-mobile carousel-img' src={infoPlaylists.images[0].url} alt={infoPlaylists.name}/>
-              <div className='carousel-text'>
-                <p className='carousel-album'>{infoPlaylists.name}</p>
+            <Link to={`/playlist/${infoPlaylists.uri}`}>
+              <div className='carousel-content'>
+                <img className='carousel-img-computer carousel-img' src={infoPlaylists.images[0].url} alt={infoPlaylists.name}/>
+                <img className='carousel-img-ipad carousel-img' src={infoPlaylists.images[0].url} alt={infoPlaylists.name}/>
+                <img className='carousel-img-mobile carousel-img' src={infoPlaylists.images[0].url} alt={infoPlaylists.name}/>
+                <div className='carousel-text'>
+                  <p className='carousel-album'>{infoPlaylists.name}</p>
+                </div>
               </div>
-            </div>
+            </Link>
           )}
         </>
       )}
--- src/pages/PlaylistSelect/index.js
+++ src/pages/PlaylistSelect/index.js
@@ -1,23 +1,45 @@
 import React, { useEffect } from 'react';
 import { useLocalStorage } from '../../hooks/useLocalStorage';
+import { useParams } from 'react-router-dom';
 import './styles.scss';
 
 function PlaylistSelect(){
+
   const [getter] = useLocalStorage();
   const accessToken = getter[getter.length - 1];
-  console.log(accessToken)
+  
+  const { playlist } = useParams();
+  const arr = playlist.split(':');
+  const type = arr[1];
+  const id = arr[2];
+  
+  console.log(type)
+  
   
   useEffect(() => {
     fetchData();
   }, []);
   
   async function fetchData(){
-    const data = await fetch('https://api.spotify.com/v1/me', {
-      headers: {
-        'Authorization': `Bearer ${accessToken}`
-      }
-    });
-    const json = await data.json();
+    if(type == 'album' || type == 'playlist' || type == 'show' || type == 'artist'){
+      const data = await fetch(`https://api.spotify.com/v1/${type}s/${id}`, {
+        headers: {
+          'Authorization': `Bearer ${accessToken}`
+        }
+      });
+      const json = await data.json();
+      console.log(json)
+      
+    }
+    if(type == 'artist'){
+      const data = await fetch(`https://api.spotify.com/v1/${type}s/${id}/top-tracks?country=SE`, {
+        headers: {
+          'Authorization': `Bearer ${accessToken}`
+        }
+      });
+      const json = await data.json();
+      //console.log(json)
+    }
   }
   return(
     <>
