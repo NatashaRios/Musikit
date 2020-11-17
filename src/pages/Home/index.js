26e6dbcbc14ef2d2a8e433c7fec2facbaf0ced1c
Natasha Rios <natu.rios@hotmail.com>
2020-11-17 17:38:24 -0300

    .

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -5,7 +5,7 @@ function Home(){
   
   return(
     <div className='home'>
-      <a className='link-login' href='https://accounts.spotify.com/es-ES/authorize?client_id=2cdf605804824d54a2aa9167bab2b9c4&redirect_uri=http:%2F%2Flocalhost:3000%2Fmusic&response_type=token&scope=user-library-modify user-follow-read playlist-read-private user-read-recently-played'>Login to Musikit</a>
+      <a className='link-login' href='https://accounts.spotify.com/es-ES/authorize?client_id=2cdf605804824d54a2aa9167bab2b9c4&redirect_uri=http:%2F%2Fmusikit.natasharios.vercel.app2Fmusic&response_type=token&scope=user-library-modify user-follow-read playlist-read-private user-read-recently-played'>Login to Musikit</a>
     </div>
   )
 }
